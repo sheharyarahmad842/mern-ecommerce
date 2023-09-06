cbf526c6841bf7117b82ff86180ab98077498ca1
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-07 01:11:43 +0500

    create header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>Welcome to Proshop</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar expand="md" bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Proshop Logo" />
+            Proshop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser /> Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
