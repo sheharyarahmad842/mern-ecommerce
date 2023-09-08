35b0615897004a4d8c2656ce306f094a12e09bb6
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-08 17:51:21 +0500

    seed sample data

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
@@ -21,5 +20,6 @@ app.get("/api/product/:id", (req, res) => {
   const product = products.find((product) => product._id === req.params.id);
   res.json(product);
 });
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
