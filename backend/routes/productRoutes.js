af347ad7312e533debea30959170f3c3d457d944
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-09 01:42:37 +0500

    error middleware and product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,11 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    // Get all products
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // Get Product by Id
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
