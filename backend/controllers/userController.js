28165d4f5676d62f1efe620c481b0ea7137130ea
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-10 01:40:56 +0500

    logout user and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,7 +42,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("Logout User");
+  //   Clear the jwt cookie
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc    Get User Profile
