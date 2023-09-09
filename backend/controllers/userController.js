5ef0112779d9f92fe2a868a6dca3fb2b4109fbd1
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-10 01:19:15 +0500

    create jwt http only cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 // @desc    Auth User
@@ -8,6 +9,16 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // Set JWT as Http-only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
     res.json({
       _id: user._id,
       name: user.name,
