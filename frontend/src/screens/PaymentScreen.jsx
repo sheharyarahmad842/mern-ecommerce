3f78985ae6a48d20152911957417ff66163a4131
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-11 15:42:20 +0500

    create payment screen

---
+++ frontend/src/screens/PaymentScreen.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { savePaymentMethod } from "../slices/cartSlice";
+import FormContainer from "../components/FormContainer";
+
+const PaymentScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddess } = cart;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  useEffect(() => {
+    if (!shippingAddess.adddress) {
+      navigate("/shipping");
+    }
+  }, [navigate, shippingAddess]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+  return (
+    <FormContainer>
+      <h1 className="text-center">Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label>Select Method</Form.Label>
+          <Form.Check
+            type="radio"
+            className="my-2"
+            name="paymentMethod"
+            label="PayPal or Credit Card"
+            value={paymentMethod}
+            checked
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          />
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
