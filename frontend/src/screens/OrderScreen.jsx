bcec7669c059f69bf325ade1c185543b86ef0d91
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-12 00:18:27 +0500

    create order page

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, Button, ListGroup, Card } from "react-bootstrap";
+import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+
+const OrderScreen = () => {
+  const { id: orderId } = useParams();
+  const { data: order, isLoading, error } = useGetOrderDetailsQuery(orderId);
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">No Order</Message>
+  ) : (
+    <>
+      <h2>Order {order._id}</h2>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>Shipping</h3>
+              <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong> {order.user.email}
+              </p>
+              <p>
+                <strong>Address: </strong> {order.shippingAddress.address},{" "}
+                {order.shippingAddress.city}, {order.shippingAddress.postalCode}
+                , {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>Payment Method</h3>
+              <p>
+                <strong>Method: </strong> {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>Order Items</h3>
+              <ListGroup variant="flush">
+                {order.orderItems.map((item, index) => (
+                  <ListGroup.Item key={index}>
+                    <Row>
+                      <Col md={1}>
+                        <Image src={item.image} alt={item.name} rounded fluid />
+                      </Col>
+                      <Col>
+                        <Link to={`/products/${item.product}`}>
+                          {item.name}
+                        </Link>
+                      </Col>
+                      <Col md={4}>
+                        {item.qty} x ${item.price} = $
+                        {(item.qty * item.price).toFixed(2)}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderScreen;
