22a7bdf7ed4e7827f069c60ab286baf96ee5e66d
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-15 00:47:20 +0500

    admin user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -115,28 +115,62 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route   GET /api/users
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("Get Users");
+  const users = await User.find({});
+  res.status(200).json(users);
 });
 
 // @desc    Get User by Id
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("Get User By Id");
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update User
 // @route   PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("Update User");
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Delete User
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("Delete User");
+  const user = await User.findById(req.params.id);
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Can't delete admin user");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: "User removed successfully" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
