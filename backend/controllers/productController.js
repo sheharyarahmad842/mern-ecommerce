15fa5c7d7d440ee8876e61c96495be28e44b4d18
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-09-15 15:42:10 +0500

    create review functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -78,10 +78,46 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc  Create a new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { comment, rating } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      comment,
+      rating: Number(rating),
+      name: req.user.name,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => acc + item.rating, 0) /
+      product.reviews.length;
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import {
+  useGetProductDetailsQuery,
+  useCreateProductReviewMutation,
+} from "../slices/productsApiSlice";
 import {
   Row,
   Col,
@@ -10,7 +13,8 @@ import {
   Card,
   Form,
 } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -19,6 +23,8 @@ import { addToCart } from "../slices/cartSlice";
 const ProductScreen = () => {
   const { id: productId } = useParams();
   const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -29,8 +35,27 @@ const ProductScreen = () => {
   const {
     data: product,
     isLoading,
+    refetch,
     error,
   } = useGetProductDetailsQuery(productId);
+  const { userInfo } = useSelector((state) => state.auth);
+  const [createProductReivew, { isLoading: loadingCreateReview }] =
+    useCreateProductReviewMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await createProductReivew({
+        productId,
+        comment,
+        rating,
+      }).unwrap();
+      refetch();
+      toast.success("Review added successfully");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
 
   return (
     <>
@@ -114,6 +139,62 @@ const ProductScreen = () => {
           </Col>
         </Row>
       )}
+      <Row>
+        <Col md={6}>
+          <h1>Reviews</h1>
+          {product?.reviews?.length === 0 && <Message>No Reviews</Message>}
+          <ListGroup variant="flush">
+            {product?.reviews.map((review) => (
+              <ListGroup.Item key={review._id}>
+                <strong>{review.name}</strong>
+                <Rating value={review.rating} />
+                <p>{review.createdAt.substring(0, 10)}</p>
+                <p>{review.comment}</p>
+              </ListGroup.Item>
+            ))}
+            <ListGroup.Item>
+              <h2>Write a customer review</h2>
+              {loadingCreateReview && <Loader />}
+              {userInfo ? (
+                <Form onSubmit={submitHandler}>
+                  <Form.Group className="my-2">
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control
+                      as="select"
+                      required
+                      value={rating}
+                      onChange={(e) => setRating(e.target.value)}
+                    >
+                      <option value="">Select...</option>
+                      <option value="1">1 - Poor</option>
+                      <option value="2">2 - Fair</option>
+                      <option value="3">3 - Good</option>
+                      <option value="4">4 - Very Good</option>
+                      <option value="5">5 - Excellent</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group className="my-2">
+                    <Form.Label>Comment</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter comment..."
+                      value={comment}
+                      onChange={(e) => setComment(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
+                  <Button type="submit" variant="primary">
+                    Add Review
+                  </Button>
+                </Form>
+              ) : (
+                <Message>
+                  Please <Link to="/login">sign in</Link> to write a review
+                </Message>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </>
   );
 };
